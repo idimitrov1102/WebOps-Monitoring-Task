feced27ea7dfc0b8cb78d159125415f3ff5a5ea2
idimitrov1102 <134808804+idimitrov1102@users.noreply.github.com>
2023-05-29 16:18:09 +0300

    Update WebOps_Monitoring_Specialist_Task.ps1

--- WebOps_Monitoring_Specialist_Task.ps1
+++ WebOps_Monitoring_Specialist_Task.ps1
@@ -12,7 +12,6 @@ $colors = @("red", "blue", "green")
 
     foreach($color in $colors){
 $folderPath = Join-Path -Path $ParentFolder -ChildPath $color
-}
 
 for ($i = 1; $i -le 50; $i++){
     $fileNumber = "{0:D2}" -f $i
@@ -20,4 +19,6 @@ for ($i = 1; $i -le 50; $i++){
     $filePath = Join-Path -Path $folderPath -ChildPath $fileName
 New-Item -ItemType File -Path $filePath -Force
 }
+
+    }
 
